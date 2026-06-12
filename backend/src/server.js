740e29cdaa17facc85f063684c3392eb0daad2f1
Abhishek Shahi <shahiabhi9900@gmail.com>
2026-06-12 09:50:12 +0530

    add user profile management with Clerk integration and middleware setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,42 @@
 import express from 'express'
 import { ENV } from './config/env.js'
 import { connectDB } from './config/db.js'
+import cors from "cors";
+import {clerkMiddleware} from "@clerk/express"
+import userRoutes from "./routes/user.route.js"
 
 const app=express()
+
+
+//middleware
+app.use(cors())
+app.use(express.json())
+app.use(c)
+
 
 app.get("/health",(req,res)=>{
     res.send("Api is Runing")
 })
 
 
-connectDB().then(()=>{
-    app.listen(ENV.PORT,()=>{
-    console.log(`Server is Runing on Port: ${ENV.PORT}`)
-})
-})
+
+//api
+app.use("/api/users",userRoutes)
+
+
+//server 
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    // listen for local development
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+    }
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
