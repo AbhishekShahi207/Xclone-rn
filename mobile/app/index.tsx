6c69029d820a2288580d239dbd74214443fd8f7d
Abhishek Shahi <shahiabhi9900@gmail.com>
2026-06-15 17:18:28 +0530

    chore: update dependencies and add Tailwind CSS configuration
    - Added babel-preset-expo and nativewind dependencies to package.json
    - Removed reset-project.js script as it is no longer needed
    - Created tailwind.config.js for Nativewind integration
    - Updated tsconfig.json to include nativewind-env.d.ts

---
+++ mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-400">
+      <Text className="text-white text-3xl">
+        NativeWind Workin
+      </Text>
+    </View>
+  );
+}
