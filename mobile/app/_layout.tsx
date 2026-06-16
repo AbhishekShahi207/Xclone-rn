359c479ed92b3b442685873f488f6cbcd8bed707
Abhishek Shahi <shahiabhi9900@gmail.com>
2026-06-17 00:32:45 +0530

    feat: enhance SearchScreen with search input and trending topics display

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout() {
       <Stack screenOptions={{
         headerShown:false
       }} >
-        <Stack.Screen name="(auth)" options={{headerShown:false}}/>
+        <Stack.Screen name="(auth)" />
       </Stack>
     </ClerkProvider>
   )
